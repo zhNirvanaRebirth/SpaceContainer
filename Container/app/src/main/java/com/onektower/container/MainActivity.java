9a1c42363935462a92603ae14c4dee91ab1d9a90
zhouhang <2501213087@qq.com>
2017-04-21 10:51:08 +0800

    project create

---
+++ Container/app/src/main/java/com/onektower/container/MainActivity.java
@@ -0,0 +1,13 @@
+package com.onektower.container;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
